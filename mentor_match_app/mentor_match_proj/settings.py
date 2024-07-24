9f669426a88a4bf5a8447a606dd9435916fd6c1b
NankyaElsa <elsakayigwa@gmail.com>
2024-07-24 15:59:06 +0300

    Added find-mentor page in the mentees app

--- mentor_match_app/mentor_match_proj/settings.py
+++ mentor_match_app/mentor_match_proj/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mentees_app',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'mentor_match_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
