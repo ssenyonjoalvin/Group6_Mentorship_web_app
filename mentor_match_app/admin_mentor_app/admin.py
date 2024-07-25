6be3512c8b80a7c2dd45a5ea0e58fb1ae7da1144
stacey5aire <staceyampaire3@gmail.com>
2024-07-25 10:36:54 +0300

    branch stacey3

--- mentor_match_app/admin_mentor_app/admin.py
+++ mentor_match_app/admin_mentor_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User, MentorshipMatch, Message, Notification, Schedule, Progress, Evaluation
 
-# Register your models here.
+# Register your models here
+admin.site.register(User)
+admin.site.register(MentorshipMatch)
+admin.site.register(Message)
+admin.site.register(Notification)
+admin.site.register(Schedule)
+admin.site.register(Progress)
+admin.site.register(Evaluation)
